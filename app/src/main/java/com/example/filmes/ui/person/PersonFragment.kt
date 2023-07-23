5f1f30b39184013c322dfb0e03341954f01bb3f8
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-07-23 16:50:09 -0300

    Show actor movies and series.

--- app/src/main/java/com/example/filmes/ui/person/PersonFragment.kt
+++ app/src/main/java/com/example/filmes/ui/person/PersonFragment.kt
@@ -1,14 +1,24 @@
 package com.example.filmes.ui.person
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.filmes.R
+import com.example.filmes.adapter.BaseViewHolder
 import com.example.filmes.databinding.FragmentPersonBinding
+import com.example.filmes.model.person.Cast
+import com.example.filmes.model.person.CastX
 import com.example.filmes.model.person.Profile
+import com.example.filmes.ui.movieDetails.MovieDetailsActivity
+import com.example.filmes.ui.seriesDetails.SerieDetailsActivity
 import com.example.filmes.views.ImageView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -50,15 +60,14 @@ class PersonFragment( private var personId: String) : BottomSheetDialogFragment(
                 binding.textPersonBiography.text = it.biography
                 binding.textPersonData.text = it.birthday
                 binding.textPersonLocal.text = it.place_of_birth
-
                 if ( it.images.profiles.isNullOrEmpty() ) binding.recyclePersonImages.visibility = View.GONE
                 else setRecyclerViewImages( it.images.profiles!! )
-
+                setRecyclerViewSeries( it.tv_credits.cast )
+                setRecyclerViewMovies( it.movie_credits.cast )
             }
         } catch (e: Exception) {
             e.printStackTrace()
         }
-
     }
 
     private fun setRecyclerViewImages(list: List<Profile>) {
@@ -73,4 +82,64 @@ class PersonFragment( private var personId: String) : BottomSheetDialogFragment(
         }
     }
 
+    private fun setRecyclerViewSeries(list: List<CastX>) {
+
+        binding.recyclerPersonSeries.apply {
+            layoutManager = LinearLayoutManager(this.context, RecyclerView.HORIZONTAL, false)
+            adapter = com.example.filmes.adapter.Adapter {
+                PersonSeriesView(it)
+            }.apply {
+                items = list.toMutableList()
+            }
+        }
+    }
+
+    private fun setRecyclerViewMovies(list: List<Cast>) {
+
+        binding.recyclerPersonMovies.apply {
+            layoutManager = LinearLayoutManager(this.context, RecyclerView.HORIZONTAL, false)
+            adapter = com.example.filmes.adapter.Adapter {
+                PersonMoviesView(it)
+            }.apply {
+                items = list.toMutableList()
+            }
+        }
+    }
+    class PersonSeriesView(viewGroup: ViewGroup) : BaseViewHolder<CastX>(
+        R.layout.movie_an_series_cell,
+        viewGroup
+    ) {
+        private val context = viewGroup.context
+        override fun bind(item: CastX) {
+            itemView.findViewById<TextView>(R.id.nomeOrTitle).text = item.name
+            if ( !item.poster_path.isNullOrEmpty() ) itemView.findViewById<android.widget.ImageView>(
+                R.id.imageViewMovieAndSeries).load( "https://image.tmdb.org/t/p/w500" + item.poster_path)
+            else itemView.findViewById<android.widget.ImageView>(R.id.imageViewMovieAndSeries).load( R.drawable.padrao)
+            itemView.findViewById<ConstraintLayout>(R.id.movieAndSeriesCellContainer).setOnClickListener {
+                val intent = Intent( context, SerieDetailsActivity::class.java ).apply {
+                    putExtra("id", item.id.toString() )
+                }
+                context.startActivity(intent)
+            }
+        }
+    }
+
+    class PersonMoviesView(viewGroup: ViewGroup) : BaseViewHolder<Cast>(
+        R.layout.movie_an_series_cell,
+        viewGroup
+    ) {
+        private val context = viewGroup.context
+        override fun bind(item: Cast) {
+            itemView.findViewById<TextView>(R.id.nomeOrTitle).text = item.title
+            if ( !item.poster_path.isNullOrEmpty() ) itemView.findViewById<android.widget.ImageView>(
+                R.id.imageViewMovieAndSeries).load( "https://image.tmdb.org/t/p/w500" + item.poster_path)
+            else itemView.findViewById<android.widget.ImageView>(R.id.imageViewMovieAndSeries).load( R.drawable.padrao)
+            itemView.findViewById<ConstraintLayout>(R.id.movieAndSeriesCellContainer).setOnClickListener {
+                val intent = Intent( context, MovieDetailsActivity::class.java ).apply {
+                    putExtra("id", item.id.toString() )
+                }
+                context.startActivity(intent)
+            }
+        }
+    }
 }
