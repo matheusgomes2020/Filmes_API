84ad60b6f39e499c54ca2fcfcd14f9a9dfaaf63f
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-07-25 16:32:48 -0300

    Splash screen.

---
+++ app/src/main/java/com/example/filmes/ui/SplashScreen.kt
@@ -0,0 +1,39 @@
+package com.example.filmes.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import com.example.filmes.MainActivity
+import com.example.filmes.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        /*
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent( this, MainActivity::class.java )
+            startActivity( intent )
+            finish()
+        },3000)
+
+
+         */
+        val backgroundImg: ImageView = findViewById(R.id.imageViewSplash)
+        val slideAnimation = AnimationUtils.loadAnimation( this, R.anim.slide )
+        backgroundImg.startAnimation( slideAnimation )
+
+        Handler().postDelayed({
+            val intent = Intent( this, MainActivity::class.java )
+            startActivity( intent )
+            finish()
+        },3000)
+
+
+    }
+}
