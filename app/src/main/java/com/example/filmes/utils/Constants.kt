0c980af2611c200b6e71db705ee90e030806d7df
Matheus Gomes <54047572+matheusgomes2020@users.noreply.github.com>
2023-08-17 17:09:33 -0300

    Update Constants.kt

--- app/src/main/java/com/example/filmes/utils/Constants.kt
+++ app/src/main/java/com/example/filmes/utils/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
 
     const val BASE_URL = "https://api.themoviedb.org/3/"
 
-    const val API_KEY = "0f5183b12ca04341d5f0f71d8bc698b5"
+    const val API_KEY = ""
 
     const val LANGUAGE = "language=pt-BR"
 }
